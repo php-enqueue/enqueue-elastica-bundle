dd16cb4c37a66c7cb86c250e1af3ca520a961c17
Bart Vanderstukken <bart.vanderstukken@calibrate.be>
2019-06-17 12:12:56 +0200

    Allow configurable message delivery delay

--- Doctrine/SyncIndexWithObjectChangeListener.php
+++ Doctrine/SyncIndexWithObjectChangeListener.php
@@ -85,6 +85,6 @@ final class SyncIndexWithObjectChangeListener implements EventSubscriber
             'repository_method' => $this->config['repository_method'],
         ]));
 
-        $this->context->createProducer()->send($queue, $message);
+        $this->context->createProducer()->setDeliveryDelay($this->config['delivery_delay'])->send($queue, $message);
     }
 }
