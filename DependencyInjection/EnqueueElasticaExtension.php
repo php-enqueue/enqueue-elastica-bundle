9306d750397980a0c730dc828976a12fabb9fde0
Alexey Kosov <kosov-alexey@yandex.ru>
2021-04-07 15:00:01 +0300

    fix a typo

--- DependencyInjection/EnqueueElasticaExtension.php
+++ DependencyInjection/EnqueueElasticaExtension.php
@@ -45,7 +45,7 @@ class EnqueueElasticaExtension extends Extension
             ->addTag('kernel.event_subscriber')
         ;
 
-        $container->register('enqueue_elastica.queue_pager_perister', QueuePagerPersister::class)
+        $container->register('enqueue_elastica.queue_pager_persister', QueuePagerPersister::class)
             ->addArgument(new Reference('enqueue_elastica.context'))
             ->addArgument(new Reference('fos_elastica.persister_registry'))
             ->addArgument(new Reference('event_dispatcher'))
